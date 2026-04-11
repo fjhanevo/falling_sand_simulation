05ed9ea194256b5587a7a629cc096b08a4873e5a
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-11 12:43:44 +0200

    added simulation files

---
+++ include/simulation.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "grid.h"
+#include "constants.h"
+#include <GLFW/glfw3.h>
+
+enum State { ACTIVE, PAUSED };
+
+class Simulation
+{
+public:
+    // Constructor 
+    Simulation(int width, int height);
+    
+    // --- Getters/setters ---
+    int getWidth() const { return m_width; }
+    int getHeight() const { return m_height; }
+    State getState() const { return m_state; }
+    void setState(State state) { m_state = state; }
+    void setWidth(int width) { m_width = width; }
+    void setHeight(int height) { m_height = height; }
+
+    // --- Simulation functions ---
+    void processInput();
+    void update();
+    void render();
+
+    // --- Public member variables ---
+    bool m_mouse_btn_left { false };
+    int m_mouseX { 0 };
+    int m_mouseY { 0 };
+    // TODO: maybe add keyboard variables
+
+private:
+    int m_width{};
+    int m_height{};
+    State m_state{ ACTIVE };
+    Grid m_grid{ GRID_WIDTH, GRID_HEIGHT };
+    //TODO: add UI element
+};
---
+++ src/simulation.cpp
@@ -0,0 +1,20 @@
+#include "simulation.h"
+#include "constants.h"
+#include <iostream>
+
+Simulation::Simulation(int width, int height)
+    : m_width(width), m_height(height)
+{}
+
+void Simulation::processInput()
+{
+    if (m_mouse_btn_left) {
+        // check if the click is inside the grid region
+        if (m_mouseX < GRID_PIXEL_W && m_mouseY < GRID_PIXEL_H) {
+            int gridX { (int) (m_mouseX / CELL_SIZE) };
+            int gridY { (int) (m_mouseY / CELL_SIZE) };
+            std::cout << "Cursor position = (" << gridX << ", " << gridY << ")\n";
+        }
+        // TODO: Add else option for UI click
+    }
+}

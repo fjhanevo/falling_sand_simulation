5f7b5b7650598a00c19dc23f5a59cb55730441de
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-12 22:33:38 +0200

    changed processInput

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,6 +1,5 @@
 #include "simulation.h"
 #include "constants.h"
-#include <iostream>
 
 Simulation::Simulation(int width, int height)
     : m_width(width), m_height(height)
@@ -10,13 +9,25 @@ void Simulation::processInput()
 {
     if (m_mouse_btn_left) {
         // check if the click is inside the grid region
-        if (m_mouseX < GRID_PIXEL_W && m_mouseY < GRID_PIXEL_H) {
-            int gridX { (int) (m_mouseX / CELL_SIZE) };
-            int gridY { (int) (m_mouseY / CELL_SIZE) };
-            m_grid.setType(gridX, gridY, m_selectedParticle);
-            std::cout << gridX << ", " << gridY << '\n';
+        int gridDisplayWidth { m_width - RIGHT_PANEL_PX };
+        int gridDisplayHeight { m_height - BOTTOM_PANEL_PX };
 
+        if (m_mouseX >= 0 && m_mouseX < gridDisplayWidth &&
+            m_mouseY >= 0 && m_mouseY < gridDisplayHeight) {
+            float normX { (float)m_mouseX / (float)gridDisplayWidth  };
+            float normY { (float)m_mouseY / (float)gridDisplayHeight };
+
+            int gridX { (int)(normX * GRID_WIDTH)  };
+            int gridY { (int)(normY * GRID_HEIGHT) };
+
+            m_grid.setType(gridX, gridY, m_selectedParticle);
         }
+        // if (m_mouseX < GRID_PIXEL_W && m_mouseY < GRID_PIXEL_H) {
+        //     int gridX { (int) (m_mouseX / CELL_SIZE) };
+        //     int gridY { (int) (m_mouseY / CELL_SIZE) };
+        //     m_grid.setType(gridX, gridY, m_selectedParticle);
+        //
+        // }
         // TODO: Add else option for UI click
     }
 }

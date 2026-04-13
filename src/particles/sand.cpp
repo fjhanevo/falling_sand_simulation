6e7ad8f27aaf9fb1ed20ff43d7e7843d526b0c26
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-13 15:42:06 +0200

    added support for sliding diagonally

--- src/particles/sand.cpp
+++ src/particles/sand.cpp
@@ -1,8 +1,21 @@
 #include "sand.h"
+#include <random>
+
+static std::mt19937 rng { std::random_device{}() };
+static std::uniform_int_distribution<int> direction{ 0, 1 };
+
 
 void updateSand(Grid& grid, int x, int y)
 {
     // Fall down
-    if (grid.isEmpty(x, y - 1))
-        grid.move(x, y - 1, x, y);
+    if (grid.isEmpty(x, y + 1))
+        grid.move(x, y + 1, x, y);
+    // check diagonally
+    else {
+        int dir { direction(rng) ? 1 : -1 };
+        if (grid.isEmpty(x + dir, y + 1))
+            grid.move(x + dir, y + 1, x, y);
+        else if (grid.isEmpty(x - dir, y + 1))
+            grid.move(x - dir, y + 1, x, y);
+    }
 } 

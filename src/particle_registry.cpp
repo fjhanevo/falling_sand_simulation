8642f36366bc480dc8a11a0a8f0efec5ee4c7dea
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-18 11:46:06 +0200

    added support for Wood

--- src/particle_registry.cpp
+++ src/particle_registry.cpp
@@ -3,6 +3,7 @@
 #include "particle.h"
 #include "sand.h"
 #include "water.h"
+#include "wood.h"
 #include <array>
 
 using UpdateFn = void(*)(Grid&, int, int);
@@ -11,6 +12,7 @@ static const UpdateFn s_updateTable[] = {
     nullptr,        // EMPTY
     updateSand,     // SAND
     updateWater,    // WATER
+    updateWood,     // WOOD
 
 };
 
@@ -27,6 +29,7 @@ static const uint8_t s_colorTable[][3] = {
     {  0,   0,   0  },  // EMPTY
     { 246, 215, 176 },  // SAND
     {  35, 137, 218 },  // WATER
+    { 139, 105,  20 },  // WOOD
 
 };
 

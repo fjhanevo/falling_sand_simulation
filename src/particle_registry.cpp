4f574b8d4d386f4a7282d889b5cb3cee7fe86cd3
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-19 12:04:03 +0200

    added support for Stone

--- src/particle_registry.cpp
+++ src/particle_registry.cpp
@@ -6,6 +6,7 @@
 #include "wood.h"
 #include "fire.h"
 #include "smoke.h"
+#include "stone.h"
 #include <algorithm>
 #include <array>
 #include <cstdint>
@@ -19,7 +20,7 @@ static const UpdateFn s_updateTable[] = {
     updateWood,     // WOOD
     updateFire,     // FIRE 
     updateSmoke,    // SMOKE
-
+    updateStone,    // STONE
 };
 
 void updateParticle(Grid &grid, int x, int y)
@@ -36,6 +37,8 @@ static const uint8_t s_colorTable[][3] = {
     { 246, 215, 176 },  // SAND
     {  35, 137, 218 },  // WATER
     { 139, 105,  20 },  // WOOD
+    { 139, 105,  20 },  // WOOD
+    {  43,  43,  42},   // STONE
 };
 
 std::array<uint8_t, 3> getParticleColor(const Grid& grid, int x, int y)

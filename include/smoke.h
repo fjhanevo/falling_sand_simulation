9b968e3c07fbd07c6e4270fcf17d77dd0ed53ea5
fjhanevo <fredrik.hanevold@gmail.com>
2026-04-19 11:29:21 +0200

    changed format in params

--- include/smoke.h
+++ include/smoke.h
@@ -2,5 +2,5 @@
 #pragma once
 #include "grid.h"
 
-void updateSmoke(Grid& grid, int x, int y);
-int  getSmokeLifetime(const Grid& grid, int x, int y);
+void updateSmoke(Grid &grid, int x, int y);
+int getSmokeLifetime(const Grid &grid, int x, int y);
